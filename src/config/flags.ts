eadbf25709d378d373d8815de2b4b90350731389
Krzysztof Modras <1228153+chrmod@users.noreply.github.com>
2025-06-02 09:53:36 +0200

    Config: rollout content script experiments

--- src/config/flags.ts
+++ src/config/flags.ts
@@ -3,12 +3,20 @@ import { Config } from "./types.ts";
 const flags: Config["flags"] = {
   "firefox-content-script-scriptlets": [
     {
-      percentage: 20,
+      percentage: 50,
       filter: {
         platform: ["firefox"],
       },
     },
   ],
+  "chromium-inject-cosmetics-on-response-started": [
+    {
+      percentage: 10,
+      filter: {
+        platform: ["chromium"],
+      },
+    },
+  ]
 };
 
 export default flags;
