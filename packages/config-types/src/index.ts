c3717af03932d3ad2b622785f6b8cb09b445b1f9
Krzysztof Modras <chrmod@chrmod.net>
2025-10-13 16:39:42 +0200

    Config types: new version

--- packages/config-types/src/index.ts
+++ packages/config-types/src/index.ts
@@ -1,3 +1,9 @@
+// Copyright 2025 Ghostery GmbH
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 export const ACTIONS = [
   "disable-antitracking-modification",
   "disable-autoconsent",
