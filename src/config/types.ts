2a1e8aa09681b3357e39019b6170799cf025c166
Krzysztof Modras <chrmod@chrmod.net>
2025-10-13 15:43:08 +0200

    Simplify tsconfig - rely on tshy defaults

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,3 +1,1 @@
-// Re-export all types from the package
-// This maintains backward compatibility while centralizing type definitions
-export * from '../../packages/config-types/src/index.ts';
+export * from "../../packages/config-types/src/index.ts";
