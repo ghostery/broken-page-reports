da9960411ed96ec1c60da8151bfc46fb0a61af57
Dominik Lubański <dominik.lubanski@gmail.com>
2026-02-17 16:02:57 +0100

    flags: separate active and comple flags

--- packages/config/src/flags.ts
+++ packages/config/src/flags.ts
@@ -4,10 +4,9 @@
 // license that can be found in the LICENSE file or at
 // https://opensource.org/licenses/MIT.
 
+
+// Active flags
 export const FLAG_CHROMIUM_INJECT_COSMETICS_ON_RESPONSE_STARTED = "chromium-inject-cosmetics-on-response-started";
-export const FLAG_DYNAMIC_DNR_FIXES = "dynamic-dnr-fixes";
-export const FLAG_EXTENDED_SELECTORS = "extended-selectors";
-export const FLAG_FIREFOX_CONTENT_SCRIPT_SCRIPTLETS = "firefox-content-script-scriptlets";
 export const FLAG_INJECTION_TARGET_DOCUMENT_ID = "injection-target-document-id";
 export const FLAG_MODES = "modes";
 export const FLAG_PAUSE_ASSISTANT = "pause-assistant";
@@ -16,11 +15,9 @@ export const FLAG_ONBOARDING_SURVEY = "onboarding-survey";
 export const FLAG_NOTIFICATION_REVIEW = "notification-review";
 export const FLAG_SUBFRAME_SCRIPTING = "subframe-scripting";
 
+// List of active flags
 export const FLAGS = [
   FLAG_CHROMIUM_INJECT_COSMETICS_ON_RESPONSE_STARTED,
-  FLAG_DYNAMIC_DNR_FIXES,
-  FLAG_EXTENDED_SELECTORS,
-  FLAG_FIREFOX_CONTENT_SCRIPT_SCRIPTLETS,
   FLAG_INJECTION_TARGET_DOCUMENT_ID,
   FLAG_MODES,
   FLAG_PAUSE_ASSISTANT,
@@ -30,8 +27,19 @@ export const FLAGS = [
   FLAG_SUBFRAME_SCRIPTING,
 ] as const;
 
-export type Flag = typeof FLAGS[number];
+// Completed flags
+export const FLAG_DYNAMIC_DNR_FIXES = "dynamic-dnr-fixes";
+export const FLAG_EXTENDED_SELECTORS = "extended-selectors";
+export const FLAG_FIREFOX_CONTENT_SCRIPT_SCRIPTLETS = "firefox-content-script-scriptlets";
+
+const COMPLETED_FLAGS = [
+  FLAG_DYNAMIC_DNR_FIXES,
+  FLAG_EXTENDED_SELECTORS,
+  FLAG_FIREFOX_CONTENT_SCRIPT_SCRIPTLETS,
+] as const;
+
+export type Flag = typeof FLAGS[number] | typeof COMPLETED_FLAGS[number];
 
 export function isFlag(flag: string): flag is Flag {
-  return (FLAGS as readonly string[]).includes(flag);
+  return (FLAGS as readonly string[]).includes(flag) || (COMPLETED_FLAGS as readonly string[]).includes(flag);
 }
